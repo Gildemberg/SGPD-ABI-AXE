d41e28448fdb90cf99945daa698f6ec49e54ca45
Gildemberg Vieira Lopes <gildembergvieiralopes@gmail.com>
2025-01-13 14:03:02 -0300

    Adicionando Procfile

--- core/settings.py
+++ core/settings.py
@@ -18,9 +18,9 @@ if not SECRET_KEY:
     SECRET_KEY = ''.join(random.choice( string.ascii_lowercase  ) for i in range( 32 ))
 
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.railway.app']
 
 # Add here your deployment HOSTS
 CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000', 'http://127.0.0.1:5085']
