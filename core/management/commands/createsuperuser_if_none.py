b13ac5555af7785961ad0547951e73c23dbf04ed
Gildemberg Vieira Lopes <gildembergvieiralopes@gmail.com>
2025-01-13 19:28:34 -0300

    teste

--- core/management/commands/createsuperuser_if_none.py
+++ core/management/commands/createsuperuser_if_none.py
@@ -5,12 +5,16 @@ class Command(BaseCommand):
     help = "Cria um superusuário se nenhum existir"
 
     def handle(self, *args, **kwargs):
+        self.stdout.write("Iniciando o comando createsuperuser_if_none...")
+
         if not User.objects.filter(is_superuser=True).exists():
             User.objects.create_superuser(
                 username="admin",
                 email="admin@example.com",
-                password="senha_segura"
+                password="admin"
             )
-            self.stdout.write("Superusuário criado com sucesso!")
+            self.stdout.write(self.style.SUCCESS("Superusuário criado com sucesso!"))
         else:
-            self.stdout.write("Superusuário já existe.")
+            self.stdout.write(self.style.WARNING("Superusuário já existe."))
+
+        self.stdout.write("Finalizando o comando createsuperuser_if_none.")
