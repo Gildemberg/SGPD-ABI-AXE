7fd1d97749f38b076c5bcfb3922f174f86d9843a
Gildemberg Vieira Lopes <gildembergvieiralopes@gmail.com>
2025-01-13 18:29:24 -0300

    teste

--- core/wsgi.py
+++ core/wsgi.py
@@ -11,15 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from django.contrib.auth.models import User
-
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser(
-        username="admin",
-        email="admin@example.com",
-        password="admin"
-    )
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()
